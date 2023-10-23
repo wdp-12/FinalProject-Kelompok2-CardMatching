a09223453afedd2d33517f3140fac05171c370a4
RafhiSaid <mrafhisaidhafiz@gmail.com>
2023-10-23 20:18:22 +0700

    add timer

---
+++ assets/js/timer.js
@@ -0,0 +1,26 @@
+let timerInterval;
+
+function startTimer() {
+    let timer = 0;
+    const timerElement = document.getElementById('timer');
+
+    function updateTimer() {
+        const hours = Math.floor(timer / 3600);
+        const minutes = Math.floor((timer % 3600) / 60);
+        const seconds = timer % 60;
+
+        const formattedTime = 
+            padZero(hours) + ':' + padZero(minutes) + ':' + padZero(seconds);
+
+        timerElement.textContent = formattedTime;
+        timer++;
+    }
+
+    timerInterval = setInterval(updateTimer, 1000);
+}
+
+function padZero(number) {
+    return (number < 10 ? '0' : '') + number;
+}
+
+startTimer();
