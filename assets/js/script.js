e6f9df731446caa31dd3b3abcaf533eab4a5a6f4
Ferdinantampubolon <tampubolonferdinan9@gmail.com>
2023-10-31 19:48:52 +0700

    Js Sound Try

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,22 @@ levelSelect.addEventListener('change', () => {
     const selectedLevel = levelSelect.value;
     window.location.href = `game.html?level=${selectedLevel}`;
 });
+
+const soundToggle = document.getElementById("soundToggle");
+const gameAudio = document.getElementById("gameAudio");
+
+let isMuted = true;
+
+soundToggle.addEventListener("click", toggleSound);
+
+function toggleSound() {
+    if (isMuted) {
+        gameAudio.play();
+        soundToggle.className = "fas fa-sharp fa-regular fa-volume-high fa-2x";
+    } else {
+        gameAudio.pause();
+        soundToggle.className = "fas fa-solid fa-volume-xmark fa-2x";
+    }
+
+    isMuted = !isMuted;
+}
